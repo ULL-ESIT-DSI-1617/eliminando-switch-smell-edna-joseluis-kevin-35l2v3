4a782355d4617085b7e7a0565df3a4525cf76d23
ednagc <ednagc.11@gmail.com>
2017-04-05 18:12:05 +0100

    Distribuir las clases en Ficheros separados closes #4

---
+++ scripts/measure.js
@@ -0,0 +1,8 @@
+"use strict";
+
+class Medida {
+	constructor(valor, medida) {
+		this.valor_ = valor;  //Valor numerico que introducimos
+		this.medida_ = medida;  //Tipo de medida (Temperatura, Longitud, etc)
+	}
+}
